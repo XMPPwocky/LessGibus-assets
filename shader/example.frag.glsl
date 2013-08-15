9e4cb6269305b26b18359349d34211f7e8e3ca68
XMPPwocky <nttheis@gmail.com>
2013-08-15 15:46:31 -0700

    Change how we pass matrices in

--- shader/example.frag.glsl
+++ shader/example.frag.glsl
@@ -1,6 +1,7 @@
 #version 330
+smooth in vec3 theColor;
 out vec4 outputColor;
 void main()
 {
-   outputColor = vec4(1.0f, 1.0f, 0.5f, 1.0f);
+   outputColor = vec4(0, gl_FragCoord.z + 1, (theColor.z),1);
 }
--- shader/example.vert.glsl
+++ shader/example.vert.glsl
@@ -4,12 +4,17 @@ layout(location = 0) in vec3 position;
 
 layout(std140) uniform GlobalMatrices
 {
+	mat4 worldToCameraMatrix;
     mat4 cameraToClipMatrix;
+	mat4 worldToClipMatrix;
 };
 
-uniform mat4 modelToCameraMatrix;
+uniform mat4 modelToClipMatrix;
+
+smooth out vec3 theColor;
 
 void main()
 {
-	gl_Position = cameraToClipMatrix * (modelToCameraMatrix * vec4(position, 1));
+	gl_Position = modelToClipMatrix * vec4(position, 1);
+	theColor = vec3(0.0f, 0.f, (position.x)+0.5);
 }
