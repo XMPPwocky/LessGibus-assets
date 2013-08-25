0036da369d129c65e4af1f48f8e22c9e37fe3c73
XMPPwocky <nttheis@gmail.com>
2013-08-24 17:00:25 -0700

    fix shaders for new attrib system

--- shader/example.vert.glsl
+++ shader/example.vert.glsl
@@ -1,20 +1,20 @@
-#version 330
-
-layout(location = 0) in vec3 position;
-
-layout(std140) uniform GlobalMatrices
-{
-	mat4 worldToCameraMatrix;
-    mat4 cameraToClipMatrix;
-	mat4 worldToClipMatrix;
-};
-
-uniform mat4 modelToClipMatrix;
-
-smooth out vec3 theColor;
-
-void main()
-{
-	gl_Position = modelToClipMatrix * vec4(position, 1);
-	theColor = vec3(0.0f, 0.f, (position.x)+0.5);
+#version 330 
+ 
+in vec3 VertexPosition; 
+ 
+layout(std140) uniform GlobalMatrices 
+{ 
+	mat4 worldToCameraMatrix; 
+  	mat4 cameraToClipMatrix; 
+	mat4 worldToClipMatrix; 
+}; 
+ 
+uniform mat4 modelToClipMatrix; 
+ 
+smooth out vec3 theColor; 
+ 
+void main() 
+{ 
+	gl_Position = modelToClipMatrix * vec4(VertexPosition, 1); 
+	theColor = vec3(0.0f, 0.f, (VertexPosition.x)+0.5); 
 }
