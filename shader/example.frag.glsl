f408e41ae8a9bb6fee7a91882709e1d328bb7f55
XMPPwocky <nttheis@gmail.com>
2013-08-31 16:07:24 -0700

    asset fixes

--- shader/example.frag.glsl
+++ shader/example.frag.glsl
@@ -2,10 +2,9 @@
 
 out vec4 outputColor;
 
-uniform sampler2D ColorTexture1;
 smooth in vec2 _ColorTexture1_uv;
 
 void main()
 {
-   outputColor = texture(ColorTexture1, _ColorTexture1_uv);
+   outputColor = vec4(0,0,0,1);
 }
